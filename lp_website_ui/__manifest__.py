ca68b9b16bf1e4ab12366206cb8cb874592390cb
tecnologialatinpyme <tecnologia@latinpymes.com>
2026-02-26 16:54:48 +0000

    Add lp_website_ui (compact form styles)

---
+++ lp_website_ui/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Latinpyme Website UI",
+    "version": "19.0.1.0.0",
+    "category": "Website",
+    "summary": "UI tweaks for website forms",
+    "depends": ["website"],
+    "data": [
+        "views/assets.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
