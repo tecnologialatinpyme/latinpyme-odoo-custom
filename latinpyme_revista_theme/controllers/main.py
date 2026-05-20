5ee85695f84d774300b90dad887cb46ed763c54c
Josimar Joel Vera Miranda <56446812+josimarveram@users.noreply.github.com>
2026-05-20 00:09:33 -0500

    Mejorar carrusel infinito de aliados en home revista

--- latinpyme_revista_theme/controllers/main.py
+++ latinpyme_revista_theme/controllers/main.py
@@ -636,7 +636,7 @@ class LatinpymeRevistaController(http.Controller):
         home_specials_banner = self._banners("home_specials", limit=1)
         home_sidebar_items = request.env["latinpyme.revista.sidebar.item"].sudo().get_active_items("home", request.website)
         home_sections = self._sections()
-        home_ally_slides = request.env["latinpyme.revista.ally"].sudo().get_active_ally_slides(request.website, 6)
+        home_allies = request.env["latinpyme.revista.ally"].sudo().get_active_allies(request.website)
         values = {
             "blog": blog,
             "sections": home_sections,
@@ -664,7 +664,7 @@ class LatinpymeRevistaController(http.Controller):
             "home_mid_banners": self._banners("home_horizontal", limit=self._home_block_limit(home_blocks.get("mid_banners"), 3)),
             "home_news_banner": home_news_banner,
             "home_specials_banner": home_specials_banner,
-            "home_ally_slides": home_ally_slides,
+            "home_allies": home_allies,
             "home_conference_item": home_sidebar_items.filtered(lambda item: item.item_type == "conference")[:1],
             "home_poll_item": home_sidebar_items.filtered(lambda item: item.item_type == "poll")[:1],
             "home_ad_item": home_sidebar_items.filtered(lambda item: item.item_type == "banner")[:1],
